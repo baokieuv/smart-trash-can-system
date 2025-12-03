66adfc327ee9b6ddf0c10b0cd1a2464659c773a1
baokieuv <154108649+baokieuv@users.noreply.github.com>
2025-12-03 21:47:51 +0700

    add thingsboard logic

--- web/smart-bin-server/src/main/java/com/example/smart_bin_server/service/DeviceService.java
+++ web/smart-bin-server/src/main/java/com/example/smart_bin_server/service/DeviceService.java
@@ -6,8 +6,15 @@ import com.example.smart_bin_server.dto.UpdateDeviceRequest;
 import com.example.smart_bin_server.mapper.DeviceMapper;
 import com.example.smart_bin_server.model.Device;
 import com.example.smart_bin_server.repository.DeviceRepository;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import okhttp3.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -15,11 +22,20 @@ public class DeviceService {
     private final DeviceRepository repository;
     private final DeviceMapper deviceMapper;
 
+    private OkHttpClient client = new OkHttpClient();
+
+    @Value("${app.thingsboard.url}")
+    private String thingsboardUrl;
+
+    private String provisionKey;
+    private String provisionSecret;
+
     public DeviceService(DeviceRepository repository, DeviceMapper deviceMapper){
         this.repository = repository;
         this.deviceMapper = deviceMapper;
     }
 
+    @Transactional
     public DeviceDto createDevice(CreateDeviceRequest request){
         Device device = new Device();
         device.setId(request.macAddress());
@@ -28,7 +44,43 @@ public class DeviceService {
                 .orElse("Device" + request.macAddress()));
         device.setOnline(false);
 
+        try {
+            device.setAccessToken(createDeviceOnThingsboard(device.getId()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         return deviceMapper.toDto(repository.save(device));
+    }
+
+    private String createDeviceOnThingsboard(String name) throws IOException {
+        String endpoint = String.format("%s/api/v1/provision", thingsboardUrl);
+
+        MediaType jsonType = MediaType.get("application/json; charset=utf-8");
+
+        String json = String.format("""
+        {
+          "deviceName": "%s",
+          "provisionDeviceKey": "%s",
+          "provisionDeviceSecret": "%s"
+        }
+        """, name, provisionKey, provisionSecret);
+
+        RequestBody body = RequestBody.create(json, jsonType);
+
+        Request request = new Request.Builder()
+                .url(endpoint)
+                .post(body)
+                .build();
+
+        String respBody;
+        try (Response response = client.newCall(request).execute()) {
+            respBody = Objects.requireNonNull(response.body()).string();
+        }
+
+        JsonObject jsonObj = JsonParser.parseString(respBody).getAsJsonObject();
+
+        return jsonObj.get("credentialsValue").getAsString();
     }
 
     public DeviceDto getDevice(String deviceId) {
@@ -38,18 +90,6 @@ public class DeviceService {
             throw new RuntimeException("Device not found");
         }
         return deviceMapper.toDto(device);
-    }
-
-    public String deleteDevice(String deviceId) {
-        Device device = repository.findById(deviceId).orElse(null);
-
-        if(device != null){
-            repository.deleteById(deviceId);
-        } else{
-            throw new RuntimeException("Device not found");
-        }
-
-        return deviceId;
     }
 
     public DeviceDto updateDevice(String deviceId, UpdateDeviceRequest request) {
@@ -63,5 +103,20 @@ public class DeviceService {
         }
 
         return deviceMapper.toDto(repository.save(device));
+    }
+
+    public String deleteDevice(String deviceId) {
+        Device device = repository.findById(deviceId).orElse(null);
+
+        if(device != null){
+            repository.deleteById(deviceId);
+        } else{
+            throw new RuntimeException("Device not found");
+        }
+
+        // Delete device on thingsboard
+        //TODO
+
+        return deviceId;
     }
 }
