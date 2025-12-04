735c3550973035c5dd4a14f2d656a50918e27aa4
baokieuv <154108649+baokieuv@users.noreply.github.com>
2025-12-04 16:41:32 +0700

    add api route classify

---
+++ web/frontend/app/api/classify/route.ts
@@ -0,0 +1,49 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+};
+
+interface Result {
+    label: string;
+    conf: number;
+    category: string;
+    error?: string | null;
+}
+
+export async function POST(req: NextRequest) {
+    const backendUrl = 'http://localhost:8000/classify';
+
+    try{
+
+        const formData = await req.formData();
+        const image = formData.get('image') as File | null;
+
+        if(!image){
+            return NextResponse.json(
+                {error: "Missing image file"},
+                {status: 400}
+            );
+        }
+        const form = new FormData();
+        form.append('image', image)
+        const response = await fetch(backendUrl, {
+            method: 'POST',
+            body: form,
+        });
+
+        if(!response.ok){
+            return NextResponse.json({ error: 'Backend error' }, { status: response.status });
+        }
+
+        const data: Result = await response.json();
+        return NextResponse.json({
+            ...data
+        }, {status: 200});
+    }catch(error){
+        console.log(error);
+        return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+    }
+}
--- web/frontend/app/page.tsx
+++ web/frontend/app/page.tsx
@@ -59,7 +59,7 @@ export default function SmartBinDashboard() {
 
   // Màn hình Dashboard chính
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-100">
+    <div className="min-h-screen bg-linear-to-br from-slate-50 to-slate-100">
       {/* Header */}
       <div className="bg-white border-b border-slate-200 sticky top-0 z-10 shadow-sm">
         <div className="max-w-7xl mx-auto px-4 md:px-8 py-4">
